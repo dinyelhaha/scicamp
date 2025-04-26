648a9ee3fda28a3b8ec5a613baa30a7b78ada484
dinyelhaha <tesorerodanielaldrin@gmail.com>
2025-04-26 15:23:13 +0800

    Delete src directory

--- src/utils/fetchData.js
+++
@@ -1,8 +0,0 @@
-fetch('data/heat-data.json')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(point => {
-      L.marker([point.lat, point.lng]).addTo(map)
-        .bindPopup(`<h3>${point.name}</h3><p>${point.description}</p>`);
-    });
-  });
